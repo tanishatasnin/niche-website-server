7bba79f9c45247c6393c1e61c2f5a6490bf1fcbe
tanishatasin <tasnintanisha@gmail.com>
2021-11-13 03:52:10 +0600

    add more

--- index.js
+++ index.js
@@ -118,6 +118,55 @@ app.get("/customers/:customerEmail", async (req, res) => {
         console.log(result);
         res.json(result);
     });
+    app.put('/users', async (req, res) => {
+        const user = req.body;
+        // console.log('put',user);
+        const filter = { email: user.email };
+        const options = { upsert: true };
+        const updateDoc = { $set: user };
+        const result = await usersCollection.updateOne(filter, updateDoc, options);
+        res.json(result);
+    });
+  
+// +++++++++++++++++++++++
+
+
+
+app.get('/users/:email', async (req, res) => {
+    const email = req.params.email;
+    const query = { email: email };
+    const user = await usersCollection.findOne(query);
+    let isAdmin = false;
+    if (user?.role === 'admin') {
+        isAdmin = true;
+    }
+    res.json({ admin: isAdmin });
+})
+
+
+
+// verifyToken, 
+    app.put('/users/admin', async (req, res) => {
+        const user = req.body;
+        console.log('put',user);
+
+        // const requester = req.decodedEmail;
+        // if (requester) {
+            // const requesterAccount = await usersCollection.findOne({ email: requester });
+        //     if (requesterAccount.role === 'admin') {
+                const filter = { email: user.email };
+                const updateDoc = { $set: { role: 'admin' } };
+                const result = await usersCollection.updateOne(filter, updateDoc);
+                res.json(result);
+        //     }
+        // }
+        // else {
+        //     res.status(403).json({ message: 'you do not have access to make admin' })
+        // }
+
+    })
+
+    // ++++++++++++++++++++++++
 
 } 
 finally{
